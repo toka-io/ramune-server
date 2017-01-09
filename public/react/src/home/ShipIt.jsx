2fcc300b1e8c4382e58c53d4d3b77435d7bcbc00
Andy Lim <andytlim@gmail.com>
2017-01-08 21:55:01 -0500

    Last commit

--- public/react/src/home/ShipIt.jsx
+++ public/react/src/home/ShipIt.jsx
@@ -1,45 +1,73 @@
 import React, { PropTypes } from 'react';
 
+const propTypes = {
+  text: PropTypes.string,
+  submitText: PropTypes.string
+};
+
+const defaultProps = {
+  regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  text: 'Ship it!',
+  submitText: 'Shipped :)'
+};
+
 class ShipIt extends React.Component {
   constructor(props) {
     super(props);
+    this.isActive = this.isActive.bind(this);
+    this.sendEmail = this.sendEmail.bind(this);
+    this.shipIt = this.shipIt.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
+    this.state = {
+      active: false,
+      disabled: true,
+      sent: false,
+      text: this.props.text
+    }
+  }
+     
+  isActive() {
+    return (this.state.active) ? ' active' : '';
+  }  
+  
+  isSent() {
+    return (this.state.sent) ? ' sent' : '';
+  }  
+  
+  sendEmail(e) {
+    e.preventDefault();
+    if(this.props.regex.test(ReactDOM.findDOMNode(this.refs.emailInput).value)) {
+      this.setState({
+        active: false,
+        sent: true,
+        text: this.props.submitText
+      });
+    }
   }
   
-  componentDidMount() {
-    $(".cta span").click(function(){
-      $(".cta:not(.sent)").addClass("active");
-      $("input").focus();
-    });
-
-    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    $(".centerMe input").on('input', function(){
-        if(regex.test($(this).val())) {
-            $("button").removeAttr("disabled"); }
-        else {
-            $("button").attr("disabled", "disabled"); }
-    });
-
-    $(".centerMe form").submit(function(x){
-        x.preventDefault();
-        if(regex.test($(".centerMe input").val())) {
-            $(".cta span").text("Shipped :)");
-            $(".cta").removeClass("active").addClass("sent");
-        }
-    });
+  shipIt() {
+    this.setState({active: true});
+    ReactDOM.findDOMNode(this.refs.shipItBtn).focus();
+  }
+  
+  validateEmail(e) {    
+    if(this.props.regex.test(e.target.value))
+      this.setState({disabled: false});
+    else
+      this.setState({disabled: true});
   }
   
   render() {
     return (
       <div className='centerMe'>
-        <div className='cta'>
-          <span>Ship it!</span>
-          <form>
+        <div className={'cta'+this.isActive()+this.isSent()} onClick={this.shipIt}>
+          <span ref="shipItBtn">{this.state.text}</span>
+          <form onSubmit={this.sendEmail}>
             <div className='input'>
-              <input placeholder='E-mail' />
+              <input placeholder='E-mail' onChange={this.validateEmail} ref="emailInput" />
             </div>
             <div className='button'>
-              <button disabled='disabled' type='submit'>Send</button>
+              <button disabled={this.state.disabled} type='submit'>Send</button>
             </div>
           </form>
         </div>
@@ -47,5 +75,8 @@ class ShipIt extends React.Component {
     );
   }
 }
+
+ShipIt.propTypes = propTypes;
+ShipIt.defaultProps = defaultProps;
 
 export default ShipIt;
--- public/react/src/home/index.jsx
+++ public/react/src/home/index.jsx
@@ -17,30 +17,6 @@ class Page extends React.Component {
     super(props);
   }
   
-  componentDidMount() {
-    $(".cta span").click(function(){
-      $(".cta:not(.sent)").addClass("active");
-      $("input").focus();
-    });
-
-    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    $(".centerMe input").on('input', function(){
-        if(regex.test($(this).val())) {
-            $("button").removeAttr("disabled"); }
-        else {
-            $("button").attr("disabled", "disabled"); }
-    });
-
-    $(".centerMe form").submit(function(x){
-        x.preventDefault();
-        if(regex.test($(".centerMe input").val())) {
-            $(".cta span").text("Shipped :)");
-            $(".cta").removeClass("active").addClass("sent");
-        }
-    });  
-  }
-  
   render() {
     var bubbles = [];
     for (var i=0; i < this.props.maxBubbles; i++) {
